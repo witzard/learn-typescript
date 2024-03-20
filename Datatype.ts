4a15d1aa76276d655dbc3362a1b67f90eb337b0e
wit_zard <wit_zard>
2024-03-20 17:59:12 +0700

    Version 2 datatypes

---
+++ Datatype.ts
@@ -0,0 +1,15 @@
+//Primitive types (Basic types): number, string, bool
+//Basic operator: + - * / 
+//Comparison operator < > = <= >=  
+
+//Number float+int
+console.log("100+1");
+console.log(100+1);
+console.log(0.001);
+//String String+char
+console.log("Hello world");
+console.log('Hello world');
+console.log(`Hello world`);
+//Bool just bool
+console.log(true);
+console.log(false);
